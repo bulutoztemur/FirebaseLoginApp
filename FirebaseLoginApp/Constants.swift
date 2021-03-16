366ecaf81e63a316fe492be3c45b78f24ff87dac
= <BulutOz@garantibbva.com.tr>
2021-03-16 23:22:01 +0300

    firebase login and sign up operations added

---
+++ FirebaseLoginApp/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  FirebaseLoginApp
+//
+//  Created by Alaattin Bulut Öztemur on 16.03.2021.
+//
+
+import Foundation
+
+
+struct Constants {
+    struct Storyboard {
+        static let homeViewController = "HomeVC"
+        
+    }
+}
--- FirebaseLoginApp/HomeViewController.swift
+++ FirebaseLoginApp/HomeViewController.swift
@@ -6,7 +6,16 @@
 //
 
 import UIKit
+import FirebaseFirestore
+import Firebase
+
 
 class HomeViewController: UIViewController {
     
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        welcomeLabel.text = "Welcome, \(Auth.auth().currentUser?.email ?? "")"
+    }
 }
